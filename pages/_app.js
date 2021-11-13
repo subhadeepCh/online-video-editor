9cf71be04b5b4d53d237e8258238a58b82bdd52b
Subhadeep Choudhuri <subhadeep.choudhuri@gmail.com>
2021-11-13 16:55:43 +0530

    CM-0 | Project-Setup | Added Files and Configurations

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  palette: {
+    primaryColor: "#ffffff",
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
