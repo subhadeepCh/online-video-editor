89c19f89b68ebd8edc2b98428d45b3e9d341fcaa
Subhadeep Choudhuri <subhadeep.choudhuri@gmail.com>
2021-11-13 23:19:25 +0530

    CM-2 | Feature: Added Appbar

--- components/dashboard/dashboard.jsx
+++ components/dashboard/dashboard.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
+import Navbar from "components/Navbar/Navbar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     color: theme.palette.primary.main,
+    width: "100%",
+    height: "100%"
   },
+  navbar: {
+      width: "100%",
+      position: "absolute",
+      top: 0
+  }
 }));
 
 const Dashboard = () => {
   const classes = useStyles();
-  return <h1 className={classes.root}>Welcome to the Dashboard</h1>;
+  return <div className={classes.root}>
+  <Navbar className={classes.navbar}/>
+  <h1>Welcome to the Dashboard</h1>
+  </div>;
 };
 
 export default Dashboard;
